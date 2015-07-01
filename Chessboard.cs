b682fe1fd8ba4e34d0ec6ff8d85484456126f420
unknown-user <unknown-user@fe80::c88f:e030:a0b0:c131%12>
2015-07-01 09:37:37 +0200

    Initial check-in of module Assembly-CSharp

---
+++ Chessboard.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chessboard : MonoBehaviour {
+
+	public int m_iSize = 10;
+	GameObject[,] m_Grid;
+
+	// Use this for initialization
+	void Start () {
+		m_Grid = new GameObject[m_iSize, m_iSize];
+
+		for (int i =0; i < m_iSize; i++)
+			for (int j =0; j < m_iSize; j++) {
+				GameObject kachel = GameObject.CreatePrimitive (PrimitiveType.Quad);
+				kachel.name = "Kachel [" + i + "] [" + j + "]";
+				m_Grid [i, j] = kachel;
+				kachel.transform.position = new Vector3 (i, j, 0);
+				kachel.transform.parent = this.transform;
+			}
+
+		Camera.main.transform.position = new Vector3 (m_iSize / 2, m_iSize / 2, -10);
+		Camera.main.orthographicSize = m_iSize;
+		transform.position = new Vector3 (0.5f, 0.5f, 0);
+
+		for (int i = 0; i < m_iSize; i++) 
+			for (int j = 0; j < m_iSize; j++) {
+				float Farben = Random.value;
+				if (Farben <= 0.5)
+					m_Grid [i, j].GetComponent<Renderer> ().material.color = Color.blue;
+			}
+			Debug.Log (GetAliveNeighbours (0, 0));
+
+	}
+
+		int GetAliveNeighbours (int _iColumn, int _iRow){
+			int AliveNeighboursCounter = 0;
+			for (int i = 0; i < 3; i++) {
+				int iTemp = i + _iColumn - 1;
+				for (int j = 0; j < 3; j++) {
+					int jTemp = j + _iRow - 1;
+					if (!(iTemp == _iColumn && jTemp == _iRow) && !(iTemp <0) && !(jTemp<0))
+						if (m_Grid [iTemp,jTemp].GetComponent<Renderer>().material.color == Color.blue)
+							AliveNeighboursCounter ++;
+						
+
+			}
+		}
+		return AliveNeighboursCounter;
+	
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
